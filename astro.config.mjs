1a7807ab3c1759a50a58aed7e20e93a75f59fec3
MOSAAB Chaouki <chaoumos@hotmail.com>
2025-05-13 23:41:06 +0200

    AstroHost: Add refined astro.config.mjs and sample content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // Placeholder, will be updated by deployment process or user
-  base: '/', 
+  site: 'https://astro9.example.com', // Replace with your actual site URL
+  base: '/', //  Replace with '/your-blog-subdirectory/' if deploying to a subdirectory
   integrations: [
-    tailwind()
+    tailwind(),
+    react()
   ],
-  // Output can be 'server' for SSR or 'static' (default)
-  // output: 'server', 
-  // adapter: node({ mode: 'standalone' }), // Example for SSR with Node
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: false
+    }
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  }
 });
